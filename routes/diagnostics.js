71ba28c87de97bce81f667f7641281b21ef00e01
Max Ohsawa <max.ohsawa@gmail.com>
2024-06-28 12:26:32 -0400

    first commit for render test

---
+++ routes/diagnostics.js
@@ -0,0 +1,23 @@
+const diagnostics = require("express").Router();
+const { v4: uuidv4 } = require("uuid");
+const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+
+// GET Route for retrieving diagnostic information
+diagnostics.get("/", (req, res) => {
+  // TODO: Logic for sending all the content of db/diagnostics.json
+  readFromFile("./db/diagnostics.json").then((data) =>
+    res.json(JSON.parse(data))
+  );
+});
+
+// POST Route for a error logging
+diagnostics.post("/", (req, res) => {
+  // TODO: Logic for appending data to the db/diagnostics.json file
+  readAndAppend(req.body, "./db/diagnostics.json");
+  return res.json({
+    message: "Added log",
+    payload: req.body,
+  });
+});
+
+module.exports = diagnostics;
